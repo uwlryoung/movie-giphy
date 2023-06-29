fc16b5265d417913d34c2d27098808ed1e8eae71
Youssef <youssefkaroui6@gmail.comm>
2023-06-28 21:00:27 -0400

    function seaarchHistoryList pretty much done, need to work on function load

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var requestMovieUrl = "http://www.omdbapi.com/?&apikey=347dfc0d&i=tt3896198";
 //user search parameters, uncomment these as you wish
 //var userGiphyInput = "";
 //var userMovieInput = "";
-
+var savedMovieNames = [];
 //this fetch is for the Giphy API
 fetch(requestGiphyUrl)
   .then(function (response) {
@@ -37,5 +37,36 @@ fetch(requestMovieUrl)
 //TODO: Function to store the local data (just movie names) onto localStorage. This should call the "render buttons" at the end of the function
 
 //TODO: Function to render the search history buttons
+
+function searchHistoryList (movieName){
+    
+
+    // creates search entry with movie name
+    var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("movie-list");
+    searchHistoryEntry.text(movieName);
+
+    // creates a container to hold search entry 
+    var entryStorageContainer = $("<div>");
+    entryStorageContainer.addClass("previous-search-container");
+
+    // appends search entry to previous search container
+    entryStorageContainer.append(searchHistoryEntry);
+
+    // append entry container to search history container
+    var searchHistoryEl = $("#search-history-container");
+    searchHistoryEl.append(searchEntryContainer);
+
+    if (savedMovieNames.length > 0){
+        // update savedSearches array with previously saved searches
+        var previousSavedMovieNames = localStorage.getItem("savedMovieNames");
+        savedMovieNames = JSON.parse(previousSavedMovieNames);
+}
+// this adds movie name to array of saved movie-names
+savedMovieNames.push(movieName);
+localStorage.setItem("savedMovieName", JSON.stringify(savedMovieNames));
+
+// resets search entry input
+$("#search-input").val("");
 
 //TODO: Function to reset the search history
